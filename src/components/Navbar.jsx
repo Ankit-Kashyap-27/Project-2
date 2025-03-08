ebf12e8c9bf31c156f1aee0b3847edf114b2c254
Ankit-Kashyap-27 <ankitkashyap73400@gmail.com>
2025-03-08 09:47:13 +0530

    upto gmt timing using temp M

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,33 @@
 import React, { Component } from 'react'
 import react from '../assets/react.svg'
-
+import moon from '../assets/moon.svg'
+import sun from '../assets/sun.svg'
+import { Link } from 'react-router-dom'
 export default class Navbar extends Component {
     render() {
         return (
-
             <nav>
                 <div className="bg-green-100 h-[10vh] flex justify-between items-center ">
-                    <div className='flex gap-[2vw]  '>
-                        <div className='text-xl font-semibold mx-5'>
+                    <div className='flex gap-[2vw] items-center'>
+                        <div className='text-xl font-semibold mx-[3vw]'>
                             <a href='/'>NEWS!HUB</a>
                         </div>
 
-                        <div className='flex gap-4'>
-                            <a href="#">HOME</a>
-                            <a href="#">ABOUT</a>
+                        <div className='flex gap-[1vw] list-none'>
+                            <li><Link className='duration-300 hover:-translate-y-1 p-2' to="/">HOME</Link></li>
+                            <li><Link className='duration-300 hover:-translate-y-1 p-2' to="/business">business</Link></li>
+                            <li><Link className='duration-300 hover:-translate-y-1 p-2' to="/entertainment">entertainment</Link></li>
+                            <li><Link className='duration-300 hover:-translate-y-1 p-2' to="/health">health</Link></li>
+                            <li><Link className='duration-300 hover:-translate-y-1 p-2' to="/science">science</Link></li>
+                            <li><Link className='duration-300 hover:-translate-y-1 p-2' to="/sports">sports</Link></li>
+                            <li><Link className='duration-300 hover:-translate-y-1 p-2' to="/technology">technology</Link></li>
+
+
                         </div>
                     </div>
-                    <div className='mx-5'>
-                        <img src={react} alt="react" />
+
+                    <div className='flex items-center mx-5 w-[5vw] h-[5vh] bg-white rounded-full'>
+                        <img className='size-6' src={sun} alt="" />
                     </div>
                 </div>
             </nav>
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,178 +1,110 @@
-import React, { Component } from 'react'
-import Newsitem from './Newsitem'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Newsitem from './Newsitem';
+import Loding from './Loding'; // corrected spelling
+
 export default class News extends Component {
+    static defaultProps = {
+        country: 'us',
+        pageSize: 6,
+        category: 'general',
+        language: 'en'
+    }
 
-
+    static propTypes = {
+        country: PropTypes.string,
+        pageSize: PropTypes.number,
+        category: PropTypes.string, // added missing propTypes
+        language: PropTypes.string // added missing propTypes
+    }
 
     constructor() {
-
         super();
-        // console.log("I am a constructor from news component")
         this.state = {
             articles: [],
-            logding: false,
-            page:1
-
-
-        }
+            loding: false, // corrected spelling
+            page: 1,
+        };
     }
 
     async componentDidMount() {
-        let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=77f8d6c72eef43508e24f5c12946d7f2"
-        let data = await fetch(url)
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=bdce1e605adb4344904784fc1343cd88&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        this.setState({ loding: true }); // corrected spelling
+        let data = await fetch(url);
         let parsedData = await data.json();
-        // console.log(parsedData)
-        this.setState({ articles: parsedData.articles ,totalResults:parsedData.totalResults})
+        this.setState({
+            articles: parsedData.articles,
+            totalResults: parsedData.totalResults,
+            loding: false, // corrected spelling
+        });
     }
 
-handleNextclick = async ()=>{
+    handleNextclick = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=bdce1e605adb4344904784fc1343cd88&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+        this.setState({ loding: true }); // corrected spelling
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState((prevState) => ({
+            page: prevState.page + 1,
+            articles: parsedData.articles,
+            loding: false, // corrected spelling
+        }));
+    }
 
+    handlePrevclick = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=bdce1e605adb4344904784fc1343cd88&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        this.setState({ loding: true }); // corrected spelling
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState((prevState) => ({
+            page: prevState.page - 1,
+            articles: parsedData.articles,
+            loding: false, // corrected spelling
+        }));
+    }
 
-
-    // console.log("NEXT",this.state.page)
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=77f8d6c72eef43508e24f5c12946d7f2&page=${this.state.page + 1}&pageSize=20`    
-    let data = await fetch(url)
-    let parsedData = await data.json();
-    // console.log(parsedData)
-    
-    
-    this.setState((prevState) => ({
-        page: prevState.page + 1,
-        articles: parsedData.articles
-    }));
-    
-
-
-}
-
-handlePrevclick = async()=>{
-    // console.log("PRIVIOUS",this.state.page)
-    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=77f8d6c72eef43508e24f5c12946d7f2&page=${this.state.page - 1}&pageSize=20`    
-    let data = await fetch(url)
-    let parsedData = await data.json();
-    // console.log(parsedData)
-
-    this.setState((prevState) => ({
-        page: prevState.page - 1,
-        articles: parsedData.articles
-    }));
-   
-}
     render() {
         return (
             <>
                 <div className=''>
+                    <h2 className='bg-white text-4xl mx-5 m-10 flex items-center justify-center font-semibold'>News!HUB - Headlines</h2>
+                    {this.state.loding && <Loding />} {/* corrected spelling */}
+                    <div className='flex justify-center items-center'>
+                        <div className='grid grid-cols-3 w-full justify-items-center'>
+                            {!this.state.loding && this.state.articles.map((element) => {
+                                return (
+                                    <div key={element.url} className=''>
+                                        <Newsitem title={element.title || ''}
 
-                    <h2 className=' bg-white text-4xl mx-5 m-10 flex items-center justify-center font-semibold'>News!HUB - Headlines</h2>
-                    <div className='flex justify-center  items-center'>
-
-                        <div className='grid grid-cols-3 w-full  justify-items-center'>
-                            {this.state.articles.map((element) => {
-
-                                return <div key={element.url} className=''>
-
-                                    <Newsitem tittle={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage ? element.urlToImage : "https://gizmodo.com/app/uploads/2025/02/GettyImages-2039371693.jpg"} newsUrl={element.url} />
-
-                                </div>
+                                            description={element.description || ''}
+                                            imageUrl={element.urlToImage || 'https://gizmodo.com/app/uploads/2025/02/GettyImages-2039371693.jpg'}
+                                            newsUrl={element.url} author={element.author} publishedAt={element.publishedAt} />
+                                    </div>
+                                );
                             })}
-
-
                         </div>
-
                     </div>
-                    <div className=' mx-auto flex justify-between items-center w-[40vw]'>
-                        
-                        <button  disabled={this.state.page<=1} type='button' className='bg-slate-900 text-white text-xl rounded-lg p-2 m-2 w-[8vw]' onClick={this.handlePrevclick} > &larr; Privous</button>
+                    <div className='mx-auto flex justify-between items-center w-[40vw]'>
+                        <button
+                            disabled={this.state.page <= 1}
+                            type='button'
+                            className={`bg-slate-900 text-white text-xl rounded-lg p-2 m-2 w-[8vw] ${this.state.page <= 1 ? 'disabled-button' : ''}`}
+                            onClick={this.handlePrevclick}
+                        >
+                            &larr; Previous
+                        </button>
 
-
-                        <button type='button' disabled={this.state.page  >= Math.ceil(this.state.totalResults/20)} className='bg-slate-900 text-white text-xl rounded-lg p-2 m-2 w-[8vw]'   onClick={this.handleNextclick} >Next &rarr;</button>
+                        <button
+                            type='button'
+                            disabled={this.state.page >= Math.ceil(this.state.totalResults / this.props.pageSize)}
+                            className={`bg-slate-900 text-white text-xl rounded-lg p-2 m-2 w-[8vw] ${this.state.page >= Math.ceil(this.state.totalResults / this.props.pageSize) ? 'disabled-button' : ''}`}
+                            onClick={this.handleNextclick}
+                        >
+                            Next &rarr;
+                        </button>
                     </div>
                 </div>
             </>
-        )
+        );
     }
 }
-
-
-
-// import React, { Component } from 'react';
-// import Newsitem from './Newsitem';
-
-// export default class News extends Component {
-//     constructor() {
-//         super();
-//         console.log("I am a constructor from news component");
-//         this.state = {
-//             articles: [],
-//             loading: false,
-//             page: 1,
-//             totalResults: 0
-//         };
-//     }
-
-//     async componentDidMount() {
-//         this.updateNews();
-//     }
-
-//     async updateNews() {
-//         this.setState({ loading: true });
-//         const { page } = this.state;
-//         const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=77f8d6c72eef43508e24f5c12946d7f2&page=${page}&pageSize=20`;
-//         try {
-//             const data = await fetch(url);
-//             const parsedData = await data.json();
-//             this.setState({
-//                 articles: parsedData.articles,
-//                 totalResults: parsedData.totalResults,
-//                 loading: false
-//             });
-//         } catch (error) {
-//             console.error("Error fetching news:", error);
-//             this.setState({ loading: false });
-//         }
-//     }
-
-//     handleNextClick = async () => {
-//         this.setState(
-//             (prevState) => ({ page: prevState.page + 1 }),
-//             () => this.updateNews()
-//         );
-//     }
-
-//     handlePrevClick = async () => {
-//         this.setState(
-//             (prevState) => ({ page: prevState.page - 1 }),
-//             () => this.updateNews()
-//         );
-//     }
-
-//     render() {
-//         const { articles, page, totalResults, loading } = this.state;
-//         return (
-//             <>
-//                 <div className=''>
-//                     <h2 className='bg-white text-4xl mx-5 m-10 flex items-center justify-center font-semibold'>News!HUB - Headlines</h2>
-//                     {loading ? (
-//                         <h3>Loading...</h3>
-//                     ) : (
-//                         <>
-//                             <div className='flex justify-center items-center'>
-//                                 <div className='grid grid-cols-3 w-full justify-items-center'>
-//                                     {articles.map((element) => (
-//                                         <div key={element.url} className=''>
-//                                             <Newsitem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage ? element.urlToImage : "https://gizmodo.com/app/uploads/2025/02/GettyImages-2039371693.jpg"} newsUrl={element.url} />
-//                                         </div>
-//                                     ))}
-//                                 </div>
-//                             </div>
-//                             <div className='mx-auto flex justify-between items-center w-[40vw]'>
-//                                 <button disabled={page <= 1} type='button' className='bg-slate-900 text-white text-xl rounded-lg p-2 m-2 w-[8vw]' onClick={this.handlePrevClick} > &larr; Previous</button>
-//                                 <button disabled={page >= Math.ceil(totalResults / 20)} type='button' className='bg-slate-900 text-white text-xl rounded-lg p-2 m-2 w-[8vw]' onClick={this.handleNextClick} >Next &rarr;</button>
-//                             </div>
-//                         </>
-//                     )}
-//                 </div>
-//             </>
-//         );
-//     }
-// }
